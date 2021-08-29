aa5154c9575eed1884dce9075a60bb71cc5a3a50
Hannes Harnisch <hannes.harnisch@gmx.net>
2021-08-29 17:08:29 +0100

    Initial commit

---
+++ Snakify/Main.cc
@@ -0,0 +1,12 @@
+﻿#include <filesystem>
+#include <fstream>
+#include <string_view>
+#include <vector>
+
+int main(int argc, char* argv[])
+{
+	using namespace std;
+	using namespace std::filesystem;
+
+	vector<string_view> v(argv, argv + argc);
+}
---
+++ premake5.lua
@@ -0,0 +1,31 @@
+output_dir				= '%{cfg.buildcfg}_%{cfg.architecture}_%{cfg.system}'
+
+workspace 'Snakify'
+	architecture		'x64'
+	configurations		{ 'Debug', 'Release' }
+	flags				{ 'MultiProcessorCompile' }
+	startproject		'Snakify'
+	language			'C++'
+	cppdialect			'C++20'
+	warnings			'Extra'
+	conformancemode		'On'
+	staticruntime		'On'
+	files				{ '**.cc', '**.hh' }
+	objdir				( '.bin_int/' .. output_dir .. '/%{prj.name}' )
+	targetdir			( '.bin/'	  .. output_dir .. '/%{prj.name}' )
+	debugdir			( '.bin/'	  .. output_dir .. '/%{prj.name}' )
+
+	filter 'configurations:Debug'
+		runtime			'Debug'
+		symbols			'On'
+		defines			'DEBUG'
+
+	filter 'configurations:Release'
+		runtime			'Release'
+		optimize		'Speed'
+		flags			{ 'LinkTimeOptimization' }
+
+project 'Snakify'
+	location			'Snakify'
+	entrypoint			'mainCRTStartup'
+	kind				'ConsoleApp'
